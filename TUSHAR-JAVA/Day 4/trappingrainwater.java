d9b8e11269a1a9b529de856e2de7b05cb625fef2
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-28 10:46:54 +0530

    Refactor Trapping Rain Water implementation by cleaning up comments and improving formatting

--- TUSHAR-JAVA/Day 4/trappingrainwater.java
+++ TUSHAR-JAVA/Day 4/trappingrainwater.java
@@ -1,37 +1,30 @@
 // !Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
- 
-
 //! Example 1:
-
-
 //! Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 //! Output: 6
 //! Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
 //! Example 2:
-
 //! Input: height = [4,2,0,3,2,5]
 //! Output: 9
 //! Explanation: The above elevation map (black section) is represented by array [4,2,0,3,2,5]. In this case, 9 units of rain water (blue section) are being trapped.
-
 //! Constraints:
-
 //! n == height.length
 //! 1 <= n <= 2 * 104
 //! 0 <= height[i] <= 105
-
 class trappingrainwater {
+
     public int trap(int[] height) {
         if (height == null || height.length < 3) {
             return 0;
         }
-        
+
         int left = 0;
         int right = height.length - 1;
         int leftMax = 0;
         int rightMax = 0;
         int water = 0;
-        
+
         while (left < right) {
             if (height[left] < height[right]) {
                 if (height[left] >= leftMax) {
@@ -49,21 +42,17 @@ class trappingrainwater {
                 right--;
             }
         }
-        
+
         return water;
     }
 
     public static void main(String[] args) {
         trappingrainwater sol = new trappingrainwater();
-        
-        // Example 1
-        int[] height1 = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println("Example 1: " + sol.trap(height1)); // Output: 6
-        
-        // Example 2
-        int[] height2 = {4,2,0,3,2,5};
-        System.out.println("Example 2: " + sol.trap(height2)); // Output: 9
+
+        int[] height1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println("Example 1: " + sol.trap(height1));
+
+        int[] height2 = {4, 2, 0, 3, 2, 5};
+        System.out.println("Example 2: " + sol.trap(height2));
     }
 }
-
-
